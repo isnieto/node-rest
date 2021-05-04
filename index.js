e7b89ad24cbe41259f41dac950625637818e00ef
Ismael Nieto <ismael.nieto@gmail.com>
2021-05-04 15:29:59 +0200

    starting level 3

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.get("/user", (req, res) => {
   res.status(200).json({ user: "Ismael", age: 18, url: url });
 });
 
+
 // - Exercici 1
 //Afegeixi una pàgina de index.html visualizable que redirigeixi a una altra about.html
 app.get("/", (req, res) => {
@@ -33,8 +34,10 @@ app.get("/", (req, res) => {
 
 app.get("/about.html", (req, res) => {
   res.status(200).sendFile(path.join(__dirname + "/public/about.html"));
-}); //
-// Exercici 2
+}); 
+
+
+/// Exercici 2
 // Executi el punt anterior utilitzant hbs (https://www.npmjs.com/package/hbs)
 const text =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500.";
@@ -56,18 +59,18 @@ app.get("/hbs/about.hbs", (req, res) => {
     link: "/hbs/",
   });
 }); //
-// Exercici 2
-// Executi el punt anterior utilitzant hbs (https://www.npmjs.com/package/hbs)
-//
+
 // Nivell 3
 // Exercici 1: Afegeixi un endpoint /upload per a pujar un fitxer al servidor de tipus png, jpg o gif i
 // retorni un missatge d'error en cas que no coincideixi amb aquestes extensions
-
 app.get("/upload", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname + "/index.html"));
 });
 
-// Para el resto no disponisble
+
+
+
+
+// Rest of routes not accesible 
 app.all("*", (req, res) => {
   res.status(404).send("<h1>Sorry, no page found</h1>");
 });
